3d1be86015d6d40877f52d65b0876590f11bd808
ArunDhoundiyal <arunprakash23797@gmail.com>
2024-01-31 07:54:08 +0000

    Create React App

---
+++ index.js
@@ -0,0 +1,13 @@
+//create-react-app
+//Installation Command:
+// It installs create-react-app globally in our environment.
+
+npm install -g create-react-app
+
+// Creating a React Application
+
+create-react-app myapp --use-npm
+
+// Starting a React Application
+
+npm start
